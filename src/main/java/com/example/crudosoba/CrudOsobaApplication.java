e8ddc99386cd23b280507befe38d779aa5f3476b
KubaKniec <jakub.kniec02@gmail.com>
2024-01-05 14:26:41 +0100

    Initial commit

---
+++ src/main/java/com/example/crudosoba/CrudOsobaApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudosoba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOsobaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudOsobaApplication.class, args);
+    }
+
+}
